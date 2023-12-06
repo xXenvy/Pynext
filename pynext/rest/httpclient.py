06740bc61399be1a85a9103ae07057caf08ba762
xXenvy <quentisxd@gmail.com>
2023-12-06 23:14:36 +0100

    Version Bump

--- pynext/rest/httpclient.py
+++ pynext/rest/httpclient.py
@@ -71,7 +71,7 @@ class HTTPClient:
         "_request_delay",
         "_timeout",
     )
-    __version__: ClassVar[str] = "1.0.8"
+    __version__: ClassVar[str] = "1.0.9"
 
     BASE_URL: ClassVar[str] = "https://discord.com/api/v10/"
 
